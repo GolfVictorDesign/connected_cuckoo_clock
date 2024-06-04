6953db31227f58a7de8ddc0cfb0812c8163ed8fb
Guillaume <guig73@gmail.com>
2024-06-04 22:07:56 +0200

    Added support for C++ in main and front_light

--- components/front_panel/front_light.h
+++ components/front_panel/front_light.h
@@ -10,7 +10,16 @@
 
 #define GPIO_FRONT_LIGHT 48
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void update_front_light(uint8_t led_red, uint8_t led_green, uint8_t led_blue);
 void configure_front_light(void);
+
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* MAIN_FRONT_PANEL_FRONT_LIGHT_H_ */
--- main/main.cpp
+++ main/main.cpp
@@ -10,11 +10,17 @@
 #include "freertos/task.h"
 #include "driver/gpio.h"
 #include "esp_log.h"
-#include "front_panel/front_light.h"
+#include "esp_wifi.h"
 #include "sdkconfig.h"
 
-void app_main(void)
+#include "wifi/wifi_connection.h"
+#include "front_panel/front_light.h"
+
+extern "C" void app_main(void)
 {
+    WifiConnection wifi;
 	configure_front_light();
 	update_front_light(45, 0, 60);
+    wifi.wifi_start();
+    wifi.wifi_connect();
 }
